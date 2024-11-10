665e579d088d84f11a05f4a47f4203cac869a0e3
NozoDev <kevinroj326@gmail.com>
2024-11-09 23:03:56 -0500

    creando nuevo empleado

--- backend/controllers/empleado.controller.js
+++ backend/controllers/empleado.controller.js
@@ -1,4 +1,22 @@
-const empleado = require("../models/empleado");
+const Empleado = require("../models/empleado");
 const empleadoCtrl = {};
 
 /**Defino los metodos para obtenera a un empleado */
+
+/**
+ * este codigo devuelde todos los empleados de la BD find busca todos los documentos en la coleccion empleado.
+ */
+empleadoCtrl.getEmpleado = async (req, res) => {
+  const empleado = await Empleado.find();
+  res.json(empleado);
+};
+
+/**Defino los metodos para crear un nuevo empleado */
+
+empleadoCrtl.createEmpleados = async (req, res) => {
+  const empleado = new Empleado(req.body);
+  await empleado.save();
+  res.json({
+    status: "empleado guardado",
+  });
+};
