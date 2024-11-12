f4a374bb7886ec421fc023d201b401e5b0cc4769
NozoDev <kevinroj326@gmail.com>
2024-11-11 19:10:33 -0500

    usuario agregado

--- client/vite-project/src/components/AgregaEmpleado.jsx
+++ client/vite-project/src/components/AgregaEmpleado.jsx
@@ -1,49 +1,112 @@
 import React from "react";
+import { addEmployed } from "../hooks/addEmployed";
 
 export const AgregaEmpleado = () => {
+  const {
+    agregarEmpleado,
+    handleInputChange,
+    handleSubmit,
+    empleados,
+    nombre,
+    apellido,
+    telefono,
+    email,
+    puesto,
+    departamento,
+    salario,
+  } = addEmployed();
+
   return (
-    <div className="">
-      <form action="">
-        <div>
+    <div className="border-1 w-full h-6/5 flex justify-center px-4 py-4">
+      <form
+        onSubmit={handleSubmit}
+        className="sm:flex sm:flex-col border-zinc-800 l w-full md:grid md:grid-cols-2 items-center border-4 px-4 rounded-md shadow-lg shadow-slate-700 gap-4"
+      >
+        <div className="flex flex-col gap-4 ">
           <label for="nombre">Nombre:</label>
-          <input type="text" id="nombre" name="nombre" required />
-        </div>
-        <div>
-          <label for="apellido">Apellido:</label>
-          <input type="text" id="apellido" name="apellido" required />
-        </div>
-
-        <div>
-          <label for="email">Email:</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-
-        <div>
-          <label for="puesto">Puesto:</label>
-          <input type="text" id="puesto" name="puesto" required />
-        </div>
-
-        <div>
-          <label for="departamento">Departamento:</label>
-          <input type="text" id="departamento" name="departamento" required />
-        </div>
-
-        <div>
-          <label for="salario">Salario:</label>
-          <input type="number" id="salario" name="salario" required />
-        </div>
-
-        <div>
-          <label for="fechaContratacion">Fecha de Contratación:</label>
           <input
-            type="date"
-            id="fechaContratacion"
-            name="fechaContratacion"
+            className="border-2 border-indigo-400 rounded-md hover:shadow-2xl hover:border-black cursor-pointer mb-4"
+            type="text"
+            id="nombre"
+            name="nombre"
+            value={nombre}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-4 ">
+          <label for="apellido">Apellido:</label>
+          <input
+            className="border-2 border-indigo-400 rounded-md hover:shadow-2xl hover:border-black cursor-pointer mb-4"
+            type="text"
+            id="apellido"
+            name="apellido"
+            value={apellido}
+            onChange={handleInputChange}
             required
           />
         </div>
 
-        <button type="submit">Agregar Empleado</button>
+        <div className="flex flex-col gap-4 ">
+          <label for="email">Email: </label>
+          <input
+            className="border-2 border-indigo-400 rounded-md hover:shadow-2xl hover:border-black cursor-pointer mb-4"
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+
+        <div className="flex flex-col gap-4 ">
+          <label for="puesto">Puesto:</label>
+          <input
+            className="border-2 border-indigo-400 rounded-md hover:shadow-2xl hover:border-black cursor-pointer mb-4"
+            type="text"
+            id="puesto"
+            name="puesto"
+            value={puesto}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+
+        <div className="flex flex-col gap-4 ">
+          <label for="departamento">Departamento:</label>
+          <input
+            className="border-2 border-indigo-400 rounded-md hover:shadow-2xl hover:border-black cursor-pointer mb-4"
+            type="text"
+            id="departamento"
+            name="departamento"
+            value={departamento}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+
+        <div className="flex flex-col gap-4 ">
+          <label for="salario">Salario:</label>
+          <input
+            className="border-2 border-indigo-400 rounded-md hover:shadow-2xl hover:border-black cursor-pointer mb-4"
+            type="number"
+            id="salario"
+            name="salario"
+            value={salario}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+
+        <div className="flex justify-center items-center col-span-2 mt-4 h-auto">
+          <button
+            className="border text-center px-4 py-4 rounded-md hover:shadow-2xl hover:shadow-indigo-400"
+            type="submit"
+          >
+            Agregar Empleado
+          </button>
+        </div>
       </form>
     </div>
   );
---
+++ client/vite-project/src/hooks/addEmployed.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { useState } from "react";
+
+export const addEmployed = () => {
+  //estado que almacena la lista de empleados
+  const [empleados, setEmpleados] = useState([]);
+
+  const [formData, setFormData] = useState({
+    nombre: "",
+    apellido: "",
+    telefono: "",
+    email: "",
+    puesto: "",
+    departamento: "",
+    salario: 0,
+  });
+
+  // función para agregar un nuevo empleado a la lista
+  const agregarEmpleado = () => {
+    const nuevoEmpleado = {
+      ...formData,
+    };
+
+    setEmpleados([...empleados, nuevoEmpleado]);
+
+    // Limpia los campos del formulario
+    setFormData({
+      nombre: "",
+      apellido: "",
+      telefono: "",
+      email: "",
+      puesto: "",
+      departamento: "",
+      salario: 0,
+    });
+  };
+
+  //desestructuramos formData para acceder a cada campo individualmente
+  const { nombre, apellido, telefono, email, puesto, departamento, salario } =
+    formData;
+
+  // manejo del input para los campos del formulario
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  //manejo del envio del formulatio
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8080/api/empleados", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log("empleado agg", data);
+
+        // limpiar el formulario
+        setFormData({
+          nombre: "",
+          apellido: "",
+          telefono: "",
+          email: "",
+          puesto: "",
+          departamento: "",
+          salario: 0,
+        });
+      } else {
+        const errorData = await response.json();
+        console.error("Error:", errorData.message);
+      }
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  };
+
+  return {
+    agregarEmpleado,
+    handleInputChange,
+    handleSubmit,
+    empleados,
+    nombre,
+    apellido,
+    telefono,
+    email,
+    puesto,
+    departamento,
+    salario,
+  };
+};
